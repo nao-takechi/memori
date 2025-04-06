2831748a34fd56804c912b0a8f94f7dc6618d14d
Nao Takechi <nao@NaonoMacBook-Air.local>
2025-04-06 15:40:48 +0900

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules, // React Hooksの推奨ルール
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ], // React Refreshのルール
+      '@typescript-eslint/no-explicit-any': 'error', // anyの使用を禁止
+    },
+  }
+)
