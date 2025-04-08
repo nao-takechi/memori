1858183e868b6c3d1602679b8e72638b0a1d0644
Nao Takechi <nao@NaonoMacBook-Air.local>
2025-04-09 00:31:53 +0900

    feat: user, diary登録

--- backend/app.js
+++ backend/app.js
@@ -1,50 +1,82 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 
 // Expressアプリのセットアップ
 const app = express();
+const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
 // LINEのチャネルアクセストークン
 const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
 
-const db = new sqlite3.Database("./db/database.db", (err) => {
-  if (err) {
-    console.error("データベース接続エラー:", err.message);
-  } else {
-    console.log("SQLiteデータベースに接続しました");
-  }
-});
-
 // Webhookのエンドポイントの設定
 app.use(bodyParser.json());
 
 // Webhookエンドポイント
-app.post("/callback", (req, res) => {
+app.post("/callback", async (req, res) => {
   const events = req.body.events;
 
   // イベントが存在する場合
   if (events && events.length > 0) {
-    events.forEach((event) => {
+    for (const event of events) {
       // メッセージイベントの場合
       if (event.type === "message" && event.message.type === "text") {
         const userMessage = event.message.text; // 受け取ったメッセージ
         const userId = event.source.userId; // ユーザーID
         const replyToken = event.replyToken; // 返信トークン
 
-        console.log(`Received message from user ${userId}: ${userMessage}`);
+        // ユーザーの確認と登録
+        await createUser(userId);
+        // 日記の登録
+        await createDiary(userId, userMessage);
 
         // ユーザーに返信する
         sendReply(replyToken, `あなたが送信したメッセージ: ${userMessage}`);
       }
-    });
+    }
   }
 
   // LINEに200 OKを返す
   res.status(200).send("OK");
 });
+
+// ユーザーが存在しない場合に新たに登録する関数
+const createUser = async (userId) => {
+  // DB にユーザーが存在するか確認
+  const user = await prisma.user.findUnique({
+    where: {
+      UID: userId, // UID が userId であるユーザーを探す
+    },
+  });
+
+  // ユーザーが DB に存在しない場合、新しく登録
+  if (!user) {
+    await prisma.user.create({
+      data: {
+        UID: userId, // 新しいユーザーとして userId を登録
+      },
+    });
+  }
+};
+
+// 日記を新規登録
+const createDiary = async (userId, message) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      UID: userId, // UID が userId であるユーザーを探す
+    },
+  });
+
+  await prisma.diary.create({
+    data: {
+      userId: user.id,
+      message: message,
+    },
+  });
+};
 
 // 返信をLINEに送信する関数
 const sendReply = (replyToken, message) => {
@@ -66,14 +98,9 @@ const sendReply = (replyToken, message) => {
   };
 
   // LINE APIにリクエストを送信
-  axios
-    .post(url, body, { headers })
-    .then((response) => {
-      console.log("Message sent:", response.data);
-    })
-    .catch((error) => {
-      console.error("Error sending message:", error);
-    });
+  axios.post(url, body, { headers }).catch((error) => {
+    console.error("Error sending message:", error);
+  });
 };
 
 // サーバーの起動
